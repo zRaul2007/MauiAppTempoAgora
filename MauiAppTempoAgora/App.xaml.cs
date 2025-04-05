25956956ab27086459a51ee194edee9b945c2580
Raul Pedrogan <rpm2007@uol.com.br>
2025-04-05 14:44:18 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppTempoAgora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
